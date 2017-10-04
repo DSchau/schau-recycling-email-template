4433d8f00c0cce46418e6e4ae81097467fd2a638
Dustin Schau <dustinschau@gmail.com>
2017-10-03 19:31:58 -0500

    chore: fix mailto bug

--- src/components/content.js
+++ src/components/content.js
@@ -18,7 +18,7 @@ export function Content({ email, message, name }) {
         </div>
         <button
           className="rounded small-expanded"
-          href={`mailto="${email}"`}
+          href={`mailto=${email}`}
           halign="middle"
         >
           Send a Reply
